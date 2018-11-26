de21c1e220e158241e5cef5d9cb886eec6f82cac
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-11-26 21:06:56 +0300

    create

---
+++ src/main/java/ru/lebedev/se/App.java
@@ -0,0 +1,13 @@
+package ru.lebedev.se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
