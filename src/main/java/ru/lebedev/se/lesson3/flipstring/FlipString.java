d815270160cca008ea9fe2cf427e16356b4e9526
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-12-03 21:24:42 +0300

    Разворачиваем строку через дек

--- src/main/java/ru/lebedev/se/lesson3/flipstring/FlipString.java
+++ src/main/java/ru/lebedev/se/lesson3/flipstring/FlipString.java
@@ -1,7 +1,29 @@
 package ru.lebedev.se.lesson3.flipstring;
 
+import ru.lebedev.se.lesson3.deque.Deque;
+import java.util.Scanner;
+
 public class FlipString {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputStr = "", outputStr = "";
+        Deque flip = new Deque(512); // Стек на базе дека
 
+        System.out.println("Введите строку для разворота:");
+        inputStr = sc.nextLine();
+
+        // Заводим в стек посимвольно
+        for (int i = 0; i < inputStr.length(); i++) {
+//            flip.insertLast(inputStr.charAt(i));
+            flip.insertFirst(inputStr.charAt(i));
+        }
+
+        // Выводим из стека посимвольно
+        for (int i = 0; i < inputStr.length(); i++) {
+//            outputStr = outputStr + flip.removeLast();
+            outputStr = outputStr + flip.removeFirst();
+        }
+        System.out.println(outputStr);
+        sc.close();
     }
 }
