21ba0767f9e98f1aca7213a743971200fa583bb4
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-12-07 00:11:11 +0300

    Очередь

--- src/main/java/ru/lebedev/se/linkedlist/queue/Queue.java
+++ src/main/java/ru/lebedev/se/linkedlist/queue/Queue.java
@@ -1,4 +1,29 @@
 package ru.lebedev.se.linkedlist.queue;
 
+import ru.lebedev.se.linkedlist.twoside.Link;
+import ru.lebedev.se.linkedlist.twoside.LinkedList;
+
 public class Queue {
+    private LinkedList queue;
+
+    public Queue() {
+        queue = new LinkedList();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public void insert(String name, int age) {
+        queue.insertFirst(name, age);
+    }
+
+    public Link delete() {
+        return queue.delete();
+    }
+
+    public void display() {
+        queue.display();
+    }
+
 }
